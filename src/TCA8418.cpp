2e71c9001b1b2f69950dbc00b8a6ec144bc435a1
James <james.waugh.r@gmail.com>
2023-08-30 20:37:01 -0400

    Dusting off: Getting I2C stuff fixed, now with DS1307 and TCA8418. Bring in library

---
+++ src/TCA8418.cpp
@@ -0,0 +1,12 @@
+#include "TCA8418.h"
+
+void TCA8418::begin() {
+  // POR
+}
+
+void TCA8418::setKeys() {
+  // Set rows / cols in KP_GPIO.
+  // Each to value of 1
+}
+
+void TCA8418::handleInterupt() {}
---
+++ src/TCA8418.h
@@ -0,0 +1,8 @@
+
+
+class TCA8418 {
+ public:
+  void begin();
+  void setKeys();
+  void handleInterupt();
+};
